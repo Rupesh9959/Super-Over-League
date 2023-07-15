2ade60b870fc9d5054699df80dfefefe35890243
Rupesh9959 <rupeshyadav9959@gmail.com>
2023-07-15 06:15:31 +0000

    Coding Practice 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="super-over-league-bg">
+    <h1 className="super-over-league-heading">Super Over League</h1>
+    <div>
+      <img
+        className="super-over-league-img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="super-over-league-img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
